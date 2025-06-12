8ccde65ffe7d07a2f983767664a04249c091f8ff
Nabil Kadouri <kadourinabil7@gmail.com>
2025-06-12 15:40:38 +0200

    feat: Ajouter les classes de enum  Genre.

---
+++ src/main/java/com/monsite/model/Genre.java
@@ -0,0 +1,20 @@
+package com.monsite.model;
+
+public enum Genre {
+    ROMAN,
+    SCIENCE_FICTION,
+    FANTASY,
+    CLASSIQUE,
+    POLICIER,
+    THRILLER,
+    POLAR,
+    BIOGRAPHIE,
+    HISTOIRE,
+    POESIE,
+    JEUNESSE,
+    DOCUMENTAIRE,
+    BANDE_DES_SIGNEES,
+    MANGA,
+    CUISINE,
+    VOYAGE
+}
